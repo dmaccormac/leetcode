8d5d95512937a3f2e239eb177544c2f4072cb83b
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-05 23:08:59 +0000

    #206 Reverse Linked List

--- java/sandbox/src/io/github/dmaccormac/Main.java
+++ java/sandbox/src/io/github/dmaccormac/Main.java
@@ -4,11 +4,17 @@ public class Main {
 
     public static void main(String args[]) {
 
-        Solution solution = new Solution();
-        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
-        var result = solution.groupAnagrams(strs);
-        System.out.println(result);
+        ListNode head = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
 
+        head.next = second;
+        second.next = third;
+        third.next = null;
+
+        Solution solution = new Solution();
+        head = solution.reverseList(head);
+        System.out.println("breakpoint");
     }
 
 }
