9dfd77e0cedc29e54f05294cfd29974505f32b5c
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-06 22:26:18 +0000

    #283 Move Zeroes

--- java/sandbox/src/io/github/dmaccormac/Main.java
+++ java/sandbox/src/io/github/dmaccormac/Main.java
@@ -4,17 +4,11 @@ public class Main {
 
     public static void main(String args[]) {
 
-        ListNode head = new ListNode(10);
-        ListNode second = new ListNode(20);
-        ListNode third = new ListNode(30);
-
-        head.next = second;
-        second.next = third;
-        third.next = null;
+        var nums = new int[] { 0, 1, 0, 3, 12 };
 
         Solution solution = new Solution();
-        head = solution.reverseList(head);
-        System.out.println("breakpoint");
+        solution.moveZeroes(nums);
+
     }
 
 }
