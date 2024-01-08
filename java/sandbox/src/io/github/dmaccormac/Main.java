c17c2797e25f1192509c30c888f002ac90fd4641
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-08 23:56:44 +0000

    #58 Length of Last Word

--- java/sandbox/src/io/github/dmaccormac/Main.java
+++ java/sandbox/src/io/github/dmaccormac/Main.java
@@ -1,16 +1,13 @@
+// https://leetcode.com/problems/length-of-last-word/
 package io.github.dmaccormac;
-
-import java.util.Arrays;
 
 public class Main {
 
     public static void main(String args[]) {
 
-        int[] nums = new int[] { 1, 2, 1 };
-
         Solution solution = new Solution();
-        var ans = solution.getConcatenation(nums);
-        System.out.println(Arrays.toString(ans));
+        var ans = solution.lengthOfLastWord("   fly me   to   the moon  ");
+        System.out.println(ans);
 
     }
 
