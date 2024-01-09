75657ab4599f96d5b132d46d803786011db1a758
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-09 01:41:53 +0000

    Initial commit

--- Solution/src/io/github/dmaccormac/Main.java
+++ Solution/src/io/github/dmaccormac/Main.java
@@ -1,10 +1,24 @@
 package io.github.dmaccormac;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String args[]) {
 
         Solution solution = new Solution();
+
+        int[] nums = new int[] { 2, 7, 11, 15 };
+        var result = solution.twoSum(nums, 9);
+        System.out.println(Arrays.toString(result));
+
+        nums = new int[] { 3, 2, 4 };
+        result = solution.twoSum(nums, 6);
+        System.out.println(Arrays.toString(result));
+
+        nums = new int[] { 3, 3 };
+        result = solution.twoSum(nums, 6);
+        System.out.println(Arrays.toString(result));
 
     }
 
--- Solution/src/io/github/dmaccormac/Solution.java
+++ Solution/src/io/github/dmaccormac/Solution.java
@@ -1,5 +1,40 @@
 package io.github.dmaccormac;
 
-class Solution {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        // Approach #1: Two pass hashmap. Time complexity O(2n)
+        // Map<Integer, Integer> map = new HashMap<>();
+
+        // // populate hashmap
+        // for (int i = 0; i < nums.length; i++)
+        // map.put(nums[i], i);
+
+        // // check if diff exists in hashmap
+        // for (int i = 0; i < nums.length; i++) {
+        // int diff = target - nums[i];
+        // if (map.containsKey(diff) && map.get(diff) != i)
+        // return new int[] { i, map.get(diff) };
+        // }
+
+        // return new int[] {}; // no solution
+
+        // Approach #2: Single hashmap. Time complexity O(n)
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // check if diff exists in hashmap.
+        // if yes, return solution. otherwise, add num + index to hashmap
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff))
+                return new int[] { map.get(diff), i };
+            map.put(nums[i], i);
+        }
+        return new int[] {}; // no solution
+
+    }
 }
