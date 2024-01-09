0bec5535b4549d17b9a44ef172827a86126ecbba
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-09 01:17:46 +0000

    Create boilerplate

--- Solution/src/io/github/dmaccormac/Main.java
+++ Solution/src/io/github/dmaccormac/Main.java
@@ -6,15 +6,6 @@ public class Main {
 
         Solution solution = new Solution();
 
-        String s = "Hello World";
-        System.out.println(solution.lengthOfLastWord(s));
-
-        s = " fly me to the moon ";
-        System.out.println(solution.lengthOfLastWord(s));
-
-        s = "luffy is still joyboy";
-        System.out.println(solution.lengthOfLastWord(s));
-
     }
 
 }
--- Solution/src/io/github/dmaccormac/Solution.java
+++ Solution/src/io/github/dmaccormac/Solution.java
@@ -1,23 +1,5 @@
 package io.github.dmaccormac;
 
 class Solution {
-    public int lengthOfLastWord(String s) {
 
-        // Approach #1: Traverse string from end
-        // Increment count for each non whitespace we hit
-        // When we hit the next whitespace, return the count
-        // Time complexity: O(n)
-        int count = 0;
-        for (int i = s.length() - 1; i > 0; i--) {
-            if (!(s.charAt(i) == ' '))
-                count++;
-
-            if ((s.charAt(i) == ' ') && (count > 0))
-                return count;
-
-        }
-
-        return count;
-
-    }
 }
