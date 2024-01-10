b070ffbb2844600b713c9b461fb338920d1eee02
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-10 01:04:41 +0000

    Initial commit

--- Solution/src/io/github/dmaccormac/Main.java
+++ Solution/src/io/github/dmaccormac/Main.java
@@ -4,7 +4,13 @@ public class Main {
 
     public static void main(String args[]) {
 
-        Solution solution = new Solution();
+        Solution s = new Solution();
+
+        int[] nums = new int[] { 3, 2, 3 };
+        System.out.println(s.majorityElement(nums));
+
+        nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+        System.out.println(s.majorityElement(nums));
 
     }
 
--- Solution/src/io/github/dmaccormac/Solution.java
+++ Solution/src/io/github/dmaccormac/Solution.java
@@ -1,5 +1,49 @@
 package io.github.dmaccormac;
 
+import java.util.Arrays;
+
 class Solution {
+
+    public int majorityElement(int[] nums) {
+
+        // Approach #1: Sorting and linear traverse
+        // Time complexity O(n log(n))
+
+        // Arrays.sort(nums);
+        // var majority = nums.length / 2;
+
+        // int count = 0;
+        // int candidate = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        // if (nums[i] != candidate) {
+        // candidate = nums[i];
+        // count = 0;
+        // }
+        // count++;
+
+        // if (count > majority)
+        // return candidate;
+
+        // }
+        // throw new IllegalStateException("Majority element not found");
+
+        // Approach #2: Moore's voting algorithm
+        // Time complexity: O(n)
+
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0)
+                candidate = num;
+
+            if (num == candidate)
+                count++;
+            else
+                count--;
+        }
+
+        return candidate;
+    }
 
 }
