479ee5d5b00555e7940f6f06e6b090e72016aa49
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-10 01:46:06 +0000

    Initial commit

--- Solution/src/io/github/dmaccormac/Main.java
+++ Solution/src/io/github/dmaccormac/Main.java
@@ -6,6 +6,15 @@ public class Main {
 
         Solution s = new Solution();
 
+        String[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        System.out.println(s.groupAnagrams(strs));
+
+        strs = new String[] { "" };
+        System.out.println(s.groupAnagrams(strs));
+
+        strs = new String[] { "a" };
+        System.out.println(s.groupAnagrams(strs));
+
     }
 
 }
