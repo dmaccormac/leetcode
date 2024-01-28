ca01ecd0b7153c92b8815b9b2fc6d735b28e7c14
dmaccormac <155025087+dmaccormac@users.noreply.github.com>
2024-01-28 00:16:09 +0000

    Update Solution.java

--- java/0001-two-sum/src/io/github/dmaccormac/Solution.java
+++ java/0001-two-sum/src/io/github/dmaccormac/Solution.java
@@ -9,6 +9,16 @@ class Solution {
 
         Map<Integer, Integer> map = new HashMap<>();
 
+        // approach - hashmap and calculate difference
+
+        // loop over each number in array and store number/index in hashmap
+        // for each num, the other value we're looking for is: target - current number
+        // if we have that other value in the hasmap, then we have found the two numbers
+        // so return index of current number and the index of other num from hashmap
+
+        // time complexity: O(n)
+        // space complexity: O(n)
+
         for (int i = 0; i < nums.length; i++) {
             int diff = target - nums[i];
             if (map.containsKey(diff))
